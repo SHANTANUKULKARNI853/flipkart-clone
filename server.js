0630b0168809a5e81e4399df0b6599d2a6426f82
SHANTANUKULKARNI853 <163411519+SHANTANUKULKARNI853@users.noreply.github.com>
2025-03-09 15:22:33 +0530

    Your commit message here

--- server.js
+++ server.js
@@ -210,5 +210,5 @@ app.post("/api/cart/remove", async (req, res) => {
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
