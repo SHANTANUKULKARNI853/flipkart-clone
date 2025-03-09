0f61213dfc09bc6df73ff988fc7473e5892bde71
SHANTANUKULKARNI853 <163411519+SHANTANUKULKARNI853@users.noreply.github.com>
2025-03-09 15:45:55 +0530

    Fixed jwt-decode import issue and updated dependencies

--- frontend/src/Pages/CategoryPage.js
+++ frontend/src/Pages/CategoryPage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import "./CategoryPage.css";
-import jwtDecode from "jwt-decode"; 
+import { jwtDecode } from "jwt-decode"; 
 
 const CategoryPage = () => {
   const { category } = useParams();
